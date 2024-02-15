23afcde8fe2aa63a2518dedcac6e24f18e6a22fd
singh-jagjot <jagjot.no1@gmail.com>
2024-02-15 07:03:29 -0500

    Added W2 ex1

---
+++ other/W2 01_Higher-order functions.scala
@@ -0,0 +1,17 @@
+import scala.annotation.tailrec
+
+def old_sum(f: Int => Int, a: Int, b: Int): Int =
+  if a > b then 0 else f(a) + old_sum(f, a + 1, b)
+
+def sum(f: Int => Int, a: Int, b: Int): Int =
+  @tailrec
+  def loop(a: Int, acc: Int): Int =
+    if a > b then acc
+    else loop(a + 1, acc + f(a))
+  loop(a, 0)
+
+@main
+def main() = {
+  println(old_sum(x => x, 1, 5))
+  println(sum(x => x, 1, 5))
+}
